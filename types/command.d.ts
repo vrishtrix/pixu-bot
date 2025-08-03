ee1bfb4e01238ff24c9f61278dd7430499c2f59e
Vrishin Patel <vrishin_patel@outlook.com>
2025-08-03 17:50:38 +0530

    add typings for commands

---
+++ types/command.d.ts
@@ -0,0 +1,16 @@
+import type { ChatInputCommandInteraction, PermissionFlags } from 'discord.js';
+import type { ConfigManager } from '@/configManager';
+
+declare global {
+	interface CommandMetadata {
+		name: string;
+		description: string;
+		requiredPermissions?: PermissionFlags[];
+		requiredFeatures?: Feature[];
+	}
+
+	interface CommandContext {
+		interaction: ChatInputCommandInteraction;
+		configManager: ConfigManager;
+	}
+}
